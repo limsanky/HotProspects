5e505901e5cedfc94abbec61fb6bc90d30a3cd78
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-08 20:56:55 +0900

    Adding feature to save the user's qr code

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAddress = "you@yoursite.come"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -27,14 +28,30 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            // save my qrcode
+                            let saver = ImageSaver()
+                            saver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
             .navigationTitle("Your Code")
+            .onAppear(perform: updateQRCode)
+            .onChange(of: name) { _ in updateQRCode() }
+            .onChange(of: emailAddress) { _ in updateQRCode() }
         }
+    }
+    
+    private func updateQRCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
     
     private func generateQRCode(from string: String) -> UIImage {
